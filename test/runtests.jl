24f5c5ef1776a09162fd62f31c4306d9559d1ba3
Shashi Gowda <shashi@juliacomputing.com>
2017-03-25 19:01:22 +0530

    0.6 compat

--- test/runtests.jl
+++ test/runtests.jl
@@ -190,7 +190,8 @@ function test_string(x1)
         "\n\nstringify: ", x2))
 end
 
-test_string("""Glob.GlobMatch(Any["base",r"h\.+"])""")
+_space = VERSION < v"0.6.0-dev" ? "" : " "
+test_string("""Glob.GlobMatch(Any["base",$(_space)r"h\.+"])""")
 test_string("""glob"base/*/a/[b]\"""")
 test_string("""fn"base/*/a/[b]\"ipedx""")
 test_string("""fn"base/*/a/[b]\"""")
