ff9aa22efc8bd7a245826bd0f021b114e913cdeb
Shuhei Kadowaki <aviatesk@gmail.com>
2025-12-05 18:34:50 +0900

    Add tests for wildcards appearing both before and after **/

--- test/runtests.jl
+++ test/runtests.jl
@@ -225,6 +225,22 @@ end
     @test occursin(fn"a*/**/test.gif"d, "ab/b/c/test.gif")
     @test !occursin(fn"a**/test.gif"d, "ab/b/h/test.gif")
 
+    # Test wildcards appearing both before and after **/
+    @test occursin(fn"a*/**/*b"d, "ax/y/z/wb")
+    @test occursin(fn"a*/**/*b"d, "ax/y/wb")
+    @test occursin(fn"a*/**/*b"d, "ax/wb")
+    @test occursin(fn"a*/**/*b"d, "a/wb")
+    @test !occursin(fn"a*/**/*b"d, "ax/y/z/w")
+    @test !occursin(fn"a*/**/*b"d, "x/y/z/wb")
+    @test occursin(fn"*a/**/*b"d, "xa/y/zb")
+    @test occursin(fn"*a/**/*b"d, "xa/zb")
+    @test occursin(fn"*a*/**/*b"d, "xaay/m/nb")
+    @test occursin(fn"a*x/**/*b"d, "ayx/z/wb")
+    @test occursin(fn"a*x/**/*b"d, "ayxx/z/wb")
+    @test occursin(fn"a*/**/b"d, "ax/y/b")
+    @test occursin(fn"a*/**/b"d, "ax/b")
+    @test !occursin(fn"a*/**/b"d, "ax/y/c")
+
     # Test ** without / (in non-pathname mode, matches any character including /)
     @test occursin(fn"a/**test.jl", "a/test.jl")
     @test occursin(fn"a/**test.jl", "a/b/test.jl")
